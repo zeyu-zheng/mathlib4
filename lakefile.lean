ea72717f85f985d1b2d9f5ee3e6b4215123ba812
Alex Keizer <alex@keizer.dev>
2023-02-24 20:05:55 +0100

    Add linter for the naming convention

--- lakefile.lean
+++ lakefile.lean
@@ -23,6 +23,8 @@ require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
 require aesop from git "https://github.com/JLimperg/aesop" @ "master"
 
+require namingLint from git "https://github.com/alexkeizer/lean-naming-lint" @ "main"
+
 lean_lib Cache where
   roots := #[`Cache]
 
