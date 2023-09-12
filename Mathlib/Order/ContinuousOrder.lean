b020921a87d76e63cb02ca68de19ab9a9ce6c26f
Christopher Hoskin <christopher.hoskin@overleaf.com>
2023-09-12 07:45:53 +0100

    bot_waybelow

--- Mathlib/Order/ContinuousOrder.lean
+++ Mathlib/Order/ContinuousOrder.lean
@@ -1,16 +1,20 @@
 import Mathlib.Order.Directed
 import Mathlib.Tactic
 
-variable (α : Type*) [Preorder α]
+variable (α : Type*)
 
 variable {α}
 
-def waybelow (U V : α) : Prop := ∀ ⦃d : Set α⦄, d.Nonempty → DirectedOn (· ≤ ·) d
+def waybelow [Preorder α] (U V : α) : Prop := ∀ ⦃d : Set α⦄, d.Nonempty → DirectedOn (· ≤ ·) d
   → ∀ ⦃a⦄, IsLUB d a → V ≤ a → ∃ W ∈ d, U ≤ W
 
-local infixl:50 " ⟪ " => waybelow
+local infixl:50 " ❰ " => waybelow
 
-lemma le_of_waybelow (U V : α) (h : U ⟪ V) : U ≤ V := by
+section Preorder
+
+variable [Preorder α]
+
+lemma le_of_waybelow (U V : α) (h : U ❰ V) : U ≤ V := by
   convert (h (Set.singleton_nonempty V) (directedOn_singleton le_refl _) isLUB_singleton
     (le_refl V))
   constructor
@@ -22,3 +26,14 @@ lemma le_of_waybelow (U V : α) (h : U ⟪ V) : U ≤ V := by
     rw [Set.mem_singleton_iff] at hW
     rw [← hW.1]
     exact hW.2
+
+lemma bot_waybelow [OrderBot α] (U : α) : ⊥ ❰ U := by
+  unfold waybelow
+  intro d hd₁ _ _ _ _
+  cases' hd₁ with W hW
+  use W
+  constructor
+  · exact hW
+  · exact bot_le
+
+end Preorder
