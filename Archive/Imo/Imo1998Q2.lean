14206a22edd9c134e7c3b25d446f012f6a331bfe
Jannis Limperg <jannis@limperg.de>
2023-11-21 12:00:21 +0100

    Archive fixes

--- Archive/Imo/Imo1998Q2.lean
+++ Archive/Imo/Imo1998Q2.lean
@@ -132,7 +132,7 @@ theorem A_fibre_over_contestant_card (c : C) :
   rw [A_fibre_over_contestant r]
   apply Finset.card_image_of_injOn
   -- porting note: used to be `tidy`. TODO: remove `ext` after `extCore` to `aesop`.
-  unfold Set.InjOn; intros; ext; all_goals aesop
+  unfold Set.InjOn; intros; ext <;> aesop
 #align imo1998_q2.A_fibre_over_contestant_card Imo1998Q2.A_fibre_over_contestant_card
 
 theorem A_fibre_over_judgePair {p : JudgePair J} (h : p.Distinct) :
@@ -140,10 +140,7 @@ theorem A_fibre_over_judgePair {p : JudgePair J} (h : p.Distinct) :
     AgreedTriple.contestant := by
   dsimp only [A, agreedContestants]; ext c; constructor <;> intro h
   · rw [Finset.mem_image]; refine' ⟨⟨c, p⟩, _⟩; aesop
-  -- porting note: this used to be `finish`
-  · simp only [Finset.mem_filter, Finset.mem_image, Prod.exists] at h
-    rcases h with ⟨_, ⟨_, ⟨_, ⟨h, _⟩⟩⟩⟩
-    cases h; aesop
+  · aesop
 #align imo1998_q2.A_fibre_over_judge_pair Imo1998Q2.A_fibre_over_judgePair
 
 theorem A_fibre_over_judgePair_card {p : JudgePair J} (h : p.Distinct) :
@@ -152,7 +149,7 @@ theorem A_fibre_over_judgePair_card {p : JudgePair J} (h : p.Distinct) :
   rw [A_fibre_over_judgePair r h]
   apply Finset.card_image_of_injOn
   -- porting note: used to be `tidy`
-  unfold Set.InjOn; intros; ext; all_goals aesop
+  unfold Set.InjOn; intros; ext <;> simp_all
 #align imo1998_q2.A_fibre_over_judge_pair_card Imo1998Q2.A_fibre_over_judgePair_card
 
 theorem A_card_upper_bound {k : ℕ}
