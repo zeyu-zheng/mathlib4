63d716e17a217bbbef49e56a9401faf4bd596f71
thorimur <68410468+thorimur@users.noreply.github.com>
2024-03-18 14:05:14 -0400

    fix `mono`

--- Mathlib/Tactic/Monotonicity/Attr.lean
+++ Mathlib/Tactic/Monotonicity/Attr.lean
@@ -25,7 +25,7 @@ syntax (name := mono) "mono" (ppSpace mono.side)? : attr
 open Lean in
 @[inherit_doc mono]
 initialize ext : LabelExtension ← (
-  let descr := "A lemma stating the monotonicity of some function, with respect to appropriate
-relations on its domain and range, and possibly with side conditions."
+  let descr := "A lemma stating the monotonicity of some function, with respect to appropriate \
+    relations on its domain and range, and possibly with side conditions."
   let mono := `mono
-  registerLabelAttr mono descr mono)
+  registerLabelAttr mono descr (ref := mono))
