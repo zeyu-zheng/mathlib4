8093fc4bbef4f249ee28d9c32c4c486dab2cbfd6
Vasily Nesterov <vas.nesterov63@gmail.com>
2025-02-19 15:27:46 +0300

    import order everywhere

--- Mathlib/Order/BoundedOrder/Lattice.lean
+++ Mathlib/Order/BoundedOrder/Lattice.lean
@@ -3,8 +3,9 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
-import Mathlib.Order.BoundedOrder.Basic
-import Mathlib.Order.Lattice
+-- import Mathlib.Order.BoundedOrder.Basic
+-- import Mathlib.Order.Lattice
+import Mathlib.Tactic.Order
 
 /-!
 # Bounded lattices
