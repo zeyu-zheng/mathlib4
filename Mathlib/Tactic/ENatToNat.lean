350da7811933f9dd276a2239c3b2c06248df2f25
Vasily Nesterov <vas.nesterov63@gmail.com>
2025-02-24 13:32:51 +0300

    import enat_to_nat everywhere

--- Mathlib/Tactic/ENatToNat.lean
+++ Mathlib/Tactic/ENatToNat.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Vasilii Nesterov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Vasilii Nesterov
 -/
-import Mathlib.Data.ENat.Basic
+import Mathlib.Data.ENat.BasicLemmas
 
 /-!
 # `enat_to_nat`
