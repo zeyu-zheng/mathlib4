41c0f16510d0a929524a6dafb54cc1ca7de8242c
Kim Morrison <kim@tqft.net>
2025-04-16 09:59:34 +1000

    fix

--- Counterexamples/SeminormLatticeNotDistrib.lean
+++ Counterexamples/SeminormLatticeNotDistrib.lean
@@ -54,20 +54,20 @@ theorem not_distrib : ¬(p ⊔ q1) ⊓ (p ⊔ q2) ≤ p ⊔ q1 ⊓ q2 := by
   rcases le_or_lt x.fst (1 / 3) with h1 | h1
   · rcases le_or_lt x.snd (2 / 3) with h2 | h2
     · calc
-        4 / 3 = 4 * (1 - 2 / 3) := by simp
+        4 / 3 = 4 * (1 - 2 / 3) := by norm_num
         _ ≤ 4 * (1 - x.snd) := by gcongr
         _ ≤ 4 * |1 - x.snd| := by gcongr; apply le_abs_self
         _ = q2 ((1, 1) - x) := rfl
         _ ≤ (p ⊔ q2) ((1, 1) - x) := le_sup_right
         _ ≤ (p ⊔ q1) x + (p ⊔ q2) ((1, 1) - x) := le_add_of_nonneg_left (apply_nonneg _ _)
     · calc
-        4 / 3 = 2 / 3 + (1 - 1 / 3) := by simp
+        4 / 3 = 2 / 3 + (1 - 1 / 3) := by norm_num
         _ ≤ x.snd + (1 - x.fst) := by gcongr
         _ ≤ |x.snd| + |1 - x.fst| := add_le_add (le_abs_self _) (le_abs_self _)
         _ ≤ p x + p ((1, 1) - x) := add_le_add le_sup_right le_sup_left
         _ ≤ (p ⊔ q1) x + (p ⊔ q2) ((1, 1) - x) := add_le_add le_sup_left le_sup_left
   · calc
-      4 / 3 = 4 * (1 / 3) := by simp
+      4 / 3 = 4 * (1 / 3) := by norm_num
       _ ≤ 4 * x.fst := by gcongr
       _ ≤ 4 * |x.fst| := by gcongr; apply le_abs_self
       _ = q1 x := rfl
