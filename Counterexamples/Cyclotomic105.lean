01d09e7e821a43f890b2692cde2043a9cefeb78e
Kim Morrison <kim@tqft.net>
2025-04-16 09:58:49 +1000

    fix

--- Counterexamples/Cyclotomic105.lean
+++ Counterexamples/Cyclotomic105.lean
@@ -24,10 +24,10 @@ namespace Counterexample
 section Computation
 
 instance Nat.fact_prime_five : Fact (Nat.Prime 5) :=
-  ⟨by simp⟩
+  ⟨by norm_num⟩
 
 instance Nat.fact_prime_seven : Fact (Nat.Prime 7) :=
-  ⟨by simp⟩
+  ⟨by norm_num⟩
 
 theorem properDivisors_15 : Nat.properDivisors 15 = {1, 3, 5} :=
   rfl
