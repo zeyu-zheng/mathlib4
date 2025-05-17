99df0a8ec7c8e82aaf562812490e4ca490a0ccc7
Yaël Dillies <yael.dillies@gmail.com>
2025-05-17 18:31:49 +0000

    shake

--- Mathlib/RingTheory/Valuation/Discrete/Basic.lean
+++ Mathlib/RingTheory/Valuation/Discrete/Basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 María Inés de Frutos-Fernández, Filippo A. E. Nuccio. All 
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: María Inés de Frutos-Fernández, Filippo A. E. Nuccio
 -/
-import Mathlib.Algebra.GroupWithZero.Int
 import Mathlib.RingTheory.Valuation.Basic
 
 /-!
