153ca260fe48901a537c20fdb7a8b1d782385758
Yaël Dillies <yael.dillies@gmail.com>
2025-05-17 18:39:16 +0000

    fix

--- Mathlib/Algebra/GroupWithZero/Int.lean
+++ Mathlib/Algebra/GroupWithZero/Int.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 María Inés de Frutos-Fernández, Filippo A. E. Nuccio. All 
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: María Inés de Frutos-Fernández, Filippo A. E. Nuccio
 -/
+import Mathlib.Algebra.Group.Int.Defs
 import Mathlib.Algebra.GroupWithZero.WithZero
 
 /-!
@@ -23,6 +24,5 @@ theorem ofAdd_zpow (a : ℤ) : (↑(ofAdd a) : ℤₘ₀) = ofAdd (1 : ℤ) ^ a 
 theorem ofAdd_neg_one_pow_comm (a : ℤ) (n : ℕ) :
     ((↑(ofAdd (-1 : ℤ)) : ℤₘ₀) ^ (-a)) ^ n = ofAdd (n : ℤ) ^ a :=
   show (exp (-1 : ℤ) ^ (-a)) ^ n = exp (n : ℤ) ^ a by simp [← exp_zsmul, ← exp_nsmul, mul_comm]
-
 
 end WithZero
